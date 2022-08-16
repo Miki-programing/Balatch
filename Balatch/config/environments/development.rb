356db76f3814926e8ea8b66cdd2c9b5582f34352
EC2 Default User <ec2-user@ip-172-31-77-27.ec2.internal>
2022-08-16 06:24:44 +0000

    apply bootstrap

--- Balatch/config/environments/development.rb
+++ Balatch/config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "98fd7848a19d456b9f89e7fd903261be.vfs.cloud9.us-east-1.amazonaws.com"
 end
