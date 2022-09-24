72840ebe7b6657b16482b7f37dfb46d1cc84aba1
Miki-programing <s911m9991@gmail.com>
2022-09-24 16:29:05 +0900

    add debit choice to pay_deposit_method_model

--- Balatch/app/models/pay_deposit_method.rb
+++ Balatch/app/models/pay_deposit_method.rb
@@ -2,6 +2,6 @@ class PayDepositMethod < ApplicationRecord
   
   has_many :histories
   
-  enum group: {cash: 0, credit: 1, online: 2}
+  enum group: {cash: 0, credit: 1, online: 2, debit: 3}
   
 end
